ce0a78949d7122002f896146a1cc18bd13a30baf
Lospel <rlaehdnl@gmail.com>
2022-11-17 11:30:06 +0900

    updated

--- app.py
+++ app.py
@@ -312,7 +312,7 @@ def sayHotIssue():
     print(body)
     print(body['userRequest']['utterance'])
 
-    url = "https://land.naver.com/news/field.naver"
+    url = "https://land.naver.com/news/hotIssue.naver"
     naver_hotissue_all(url)
 
     responseHotIssue = {
